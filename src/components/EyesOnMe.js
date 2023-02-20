e299597f2a4d536790a7b0588ee6cc1b79b9b98c
GeralmyUrena <geralmyuri11@gmail.com>
2023-02-20 16:34:35 -0500

    passed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return (
+        <button onFocus ={e => console.log("Good!")}  onBlur ={event => console.log("Hey! Eyes on me!")}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
